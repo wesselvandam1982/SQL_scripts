02bee0800cb44adfe951e30e049c7f3efb3d5588
Wessel van Dam <49208873+wesselvandam1982@users.noreply.github.com>
2019-09-30 16:56:00 -0400

    Update top_spenders_by_country.sql

--- top_spenders_by_country.sql
+++ top_spenders_by_country.sql
@@ -1,6 +1,5 @@
 /*
-
-Write a query that determines the customer that has spent the most on music for each country.
+Query that determines the customer that has spent the most on music for each country.
 Write a query that returns the country along with the top customer and how much they spent.
 For countries where the top amount spent is shared, provide all customers who spent this amount.
 
